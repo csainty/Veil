c6c517ccbebb4778f2a5e138cfc5db495080a073
Chris Sainty <csainty@hotmail.com>
2014-03-22 13:06:04 +0100

    Added test to ensure every node in the syntax tree is processed

---
+++ Src/Veil.Tests/Compiler/TemplateRootNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Veil.Compiler
+{
+    [TestFixture]
+    internal class TemplateRootNodeTests : CompilerTestBase
+    {
+        [Test]
+        public void Should_emit_all_nodes_in_syntax_tree()
+        {
+            var model = new { Name = "World" };
+            var template = CreateTemplate(
+                new WriteLiteralNode { LiteralType = typeof(string), LiteralContent = "Hello " },
+                new WriteModelPropertyNode { ModelProperty = model.GetType().GetProperty("Name") },
+                new WriteLiteralNode { LiteralType = typeof(string), LiteralContent = "!" }
+            );
+            var result = ExecuteTemplate(template, model);
+            Assert.That(result, Is.EqualTo("Hello World!"));
+        }
+    }
+}
