31ce28be26c8bce883eee522861d4f689527c86a
Chris Sainty <csainty@hotmail.com>
2014-03-22 12:32:09 +0100

    Added Sigil and nUnit. nUnit has been chosen because it does not require text fixtures to be public, which allows keeping the public surface of the library small.

---
+++ Src/Veil/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Veil
+{
+    internal static class Extensions
+    {
+        public static string FormatInvariant(this string format, params object[] args)
+        {
+            return String.Format(CultureInfo.InvariantCulture, format, args);
+        }
+    }
+}
