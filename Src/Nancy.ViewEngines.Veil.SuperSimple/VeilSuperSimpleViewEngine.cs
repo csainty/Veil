f3fd74eb86dacb7594e7f08a94ab40d40b135090
Chris Sainty <csainty@hotmail.com>
2014-05-03 10:31:52 +0200

    Removed unused code from VeilSuperSimpleViewEngine

--- Src/Nancy.ViewEngines.Veil.SuperSimple/VeilSuperSimpleViewEngine.cs
+++ Src/Nancy.ViewEngines.Veil.SuperSimple/VeilSuperSimpleViewEngine.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Veil;
 using Veil.SuperSimple;
 
@@ -9,13 +8,13 @@ namespace Nancy.ViewEngines.Veil.SuperSimple
 {
     public class VeilSuperSimpleViewEngine : IViewEngine
     {
+        private const string EngineKey = "supersimple";
         private IVeilContext context;
         private IVeilEngine engine;
-        private static MethodInfo compileMethod = typeof(IVeilEngine).GetMethod("Compile");
 
         static VeilSuperSimpleViewEngine()
         {
-            VeilEngine.RegisterParser("supersimple", new SuperSimpleParser());
+            VeilEngine.RegisterParser(EngineKey, new SuperSimpleParser());
         }
 
         public IEnumerable<string> Extensions
@@ -34,7 +33,7 @@ namespace Nancy.ViewEngines.Veil.SuperSimple
             var template = renderContext.ViewCache.GetOrAdd(viewLocationResult, result =>
             {
                 Type modelType = model.GetType();
-                return this.engine.CompileNonGeneric("supersimple", result.Contents(), modelType);
+                return this.engine.CompileNonGeneric(EngineKey, result.Contents(), modelType);
             });
 
             var response = new Response();
