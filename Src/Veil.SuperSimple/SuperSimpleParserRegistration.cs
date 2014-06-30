d05206c7baa836e8aba521197d79c70b792e4caa
Chris Sainty <csainty@hotmail.com>
2014-06-30 19:42:47 +0200

    Renamed the SuperSimple parser registration so it sits more naturally with the file it is for

--- Src/Veil.SuperSimple/SuperSimpleParserRegistration.cs
+++ Src/Veil.SuperSimple/SuperSimpleParserRegistration.cs
@@ -7,7 +7,7 @@ namespace Veil.SuperSimple
     /// <summary>
     /// Used to auto-register this parser. You should not need touch it.
     /// </summary>
-    public class SuperSimpleTemplateParserRegistration : ITemplateParserRegistration
+    public class SuperSimpleParserRegistration : ITemplateParserRegistration
     {
         public IEnumerable<string> Keys
         {
