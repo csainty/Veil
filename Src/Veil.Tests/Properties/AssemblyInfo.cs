eb1ff0decf9a38f2ce00100f0318f4fb077acf6d
Chris Sainty <csainty@hotmail.com>
2014-07-05 14:49:07 +0200

    Build v0.2.0

--- Src/Veil.Tests/Properties/AssemblyInfo.cs
+++ Src/Veil.Tests/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cccbb679-aaec-4561-8e87-fb798080b349")]
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
