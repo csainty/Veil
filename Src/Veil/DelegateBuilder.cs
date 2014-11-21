2ecc66e41eec4aa9eaf5669cf99bff65c7bc18b6
Chris Sainty <csainty@hotmail.com>
2014-11-21 22:11:36 +0100

    Update DelegateBuilder to use linq expressions also

--- Src/Veil/DelegateBuilder.cs
+++ Src/Veil/DelegateBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Linq;
 
 namespace Veil
 {
@@ -8,83 +10,48 @@ namespace Veil
     {
         public static Func<object, object> FunctionCall(Type modelType, MethodInfo function)
         {
-            var method = new DynamicMethod("DelegateBuilder_FunctionCall_{0}_{1}".FormatInvariant(modelType.Name, function.Name), typeof(object), new[] { typeof(object) }, true);
-            var il = method.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Castclass, modelType);
-
-            if (function.IsVirtual)
-            {
-                il.Emit(OpCodes.Callvirt, function);
-            }
-            else
-            {
-                il.Emit(OpCodes.Call, function);
-            }
-            if (function.ReturnType.IsValueType)
-            {
-                il.Emit(OpCodes.Box, function.ReturnType);
-            }
-            il.Emit(OpCodes.Ret);
-
-            return (Func<object, object>)method.CreateDelegate(typeof(Func<object, object>));
+            var model = Expression.Parameter(typeof(object));
+            var castModel = Expression.Convert(model, modelType);
+            var call = Expression.Call(castModel, function);
+            return Expression.Lambda<Func<object, object>>(
+                Expression.Convert(call, typeof(object)),
+                model
+            ).Compile();
         }
 
         public static Func<object, object> Property(Type modelType, PropertyInfo property)
         {
-            var getter = property.GetGetMethod();
-
-            var method = new DynamicMethod("DelegateBuilder_Property_{0}_{1}".FormatInvariant(modelType.Name, property.Name), typeof(object), new[] { typeof(object) }, true);
-            var il = method.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Castclass, modelType);
-
-            if (getter.IsVirtual)
-            {
-                il.Emit(OpCodes.Callvirt, getter);
-            }
-            else
-            {
-                il.Emit(OpCodes.Call, getter);
-            }
-
-            if (property.PropertyType.IsValueType)
-            {
-                il.Emit(OpCodes.Box, property.PropertyType);
-            }
-            il.Emit(OpCodes.Ret);
-
-            return (Func<object, object>)method.CreateDelegate(typeof(Func<object, object>));
+            var model = Expression.Parameter(typeof(object));
+            var castModel = Expression.Convert(model, modelType);
+            var call = Expression.Property(castModel, property);
+            return Expression.Lambda<Func<object, object>>(
+                Expression.Convert(call, typeof(object)),
+                model
+            ).Compile();
         }
 
         public static Func<object, object> Field(Type modelType, FieldInfo field)
         {
-            var method = new DynamicMethod("DelegateBuilder_Field_{0}_{1}".FormatInvariant(modelType.Name, field.Name), typeof(object), new[] { typeof(object) }, true);
-            var il = method.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Castclass, modelType);
-            il.Emit(OpCodes.Ldfld, field);
-            if (field.FieldType.IsValueType) il.Emit(OpCodes.Box, field.FieldType);
-            il.Emit(OpCodes.Ret);
-
-            return (Func<object, object>)method.CreateDelegate(typeof(Func<object, object>));
+            var model = Expression.Parameter(typeof(object));
+            var castModel = Expression.Convert(model, modelType);
+            var call = Expression.Field(castModel, field);
+            return Expression.Lambda<Func<object, object>>(
+                Expression.Convert(call, typeof(object)),
+                model
+            ).Compile();
         }
 
         public static Func<object, object> Dictionary(Type modelType, string key)
         {
-            var getItem = modelType.GetMethod("get_Item");
-            var method = new DynamicMethod("DelegateBuilder_Dictionary_{0}_{1}".FormatInvariant(modelType.Name, key), typeof(object), new[] { typeof(object) }, true);
-            var il = method.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Castclass, modelType);
-            il.Emit(OpCodes.Ldstr, key);
-            if (getItem.IsVirtual)
-                il.Emit(OpCodes.Callvirt, getItem);
-            else
-                il.Emit(OpCodes.Call, getItem);
-            il.Emit(OpCodes.Ret);
+            var model = Expression.Parameter(typeof(object));
+            var castModel = Expression.Convert(model, modelType);
+            var indexProperty = modelType.GetProperties().First(x => x.GetIndexParameters().Length == 1);
+            var call = Expression.MakeIndex(castModel, indexProperty, new[] { Expression.Constant(key) });
 
-            return (Func<object, object>)method.CreateDelegate(typeof(Func<object, object>));
+            return Expression.Lambda<Func<object, object>>(
+                Expression.Convert(call, typeof(object)),
+                model
+            ).Compile();
         }
     }
 }
